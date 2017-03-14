0e477dd58ff382da646f9beb0aff2dd603e511cb
Shubhaprada-26 <shubhaprada26@gmail.com>
2017-03-14 22:18:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
+
+var config={
+    user:'shubhaprada-26',
+    database:'shubhaprada-26',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
+
+var pool=new pool(config);
+app.get('/test-db',function(req,res){
+  pool.query('select * from test',function(err,res){
+      if(err){
+          res.status('500').send(err.toString());
+      }else
+      {
+          res.send(JSON.stringfy(result));
+      }
+  }  );
+});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
